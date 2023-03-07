852d9c737d190492e9fd235044feec99266e763e
Yahya9102 <112933451+Yahya9102@users.noreply.github.com>
2023-03-07 10:59:06 +0100

    lägger till fler kommentarer samt fixar till namn på ett antal variablar

--- src/test/java/com/example/produktapi/service/SeleniumTest.java
+++ src/test/java/com/example/produktapi/service/SeleniumTest.java
@@ -54,6 +54,7 @@ public class SeleniumTest {
     @Test //För tajta tshirten
     public void testProductPriceForMensCasualSlimFitT_shirt() {
 
+        // Hitta elementet med hjälp av dess XPath
         WebElement priceElement = new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[contains(text(), 'Vilken härlig t-shirt, slim fit o casual i ett!')]")));
 
@@ -70,6 +71,7 @@ public class SeleniumTest {
     @Test //pris för jackan
     public void testProductPriceForMensCottonJacket() {
 
+        // Hitta elementet med hjälp av dess XPath
         WebElement priceElement = new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[contains(text(), 'Bomullsjacka, vad finns det mer att säga?!')]")));
 
@@ -86,6 +88,7 @@ public class SeleniumTest {
     @Test //pris för armbandet
     public void testProductPriceForJohnHardyBracelet() {
 
+        // Hitta elementet med hjälp av dess XPath
         WebElement priceElement = new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[contains(text(), 'Silver drakens återkomst. Ett måste om man vill ha den!')]")));
 
@@ -102,6 +105,7 @@ public class SeleniumTest {
     @Test //pris för dator skärmen
     public void testProductPriceForAcerPcScreen() {
 
+        // Hitta elementet med hjälp av dess XPath
         WebElement priceElement = new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[contains(text(), 'Full HD, fan va coolt. Den ska vi ha!')]")));
 
@@ -118,6 +122,7 @@ public class SeleniumTest {
     @Test //pris för väskan
     public void testProductPriceForFjallRavenBackPack() {
 
+        // Hitta elementet med hjälp av dess XPath
         WebElement priceElement = new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[contains(text(), 'Fin väska me plats för dator')]")));
 
@@ -137,6 +142,7 @@ public class SeleniumTest {
 
         driver.navigate().to("https://java22.netlify.app/");
 
+        // Hitta elementet med hjälp av dess XPath
         WebElement priceElement = new WebDriverWait(driver, Duration.ofSeconds(20))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[contains(text(), 'Snygg och bara att sätta på sig om man har köpt rätt storlek')]")));
 
@@ -153,7 +159,7 @@ public class SeleniumTest {
     @Test //Test för bild src av väskan
     public void testImageShowingForItemFjallravenBagpack() {
 
-
+        // Hitta bild-elementet med hjälp av dess XPath
         WebElement productImage = new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[@class='card-img-top' and contains(@src, 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg')]")));
 
@@ -167,6 +173,7 @@ public class SeleniumTest {
     @Test  //Test för bild src av slimfit plaggen
     public void testImageShowingForMensCasualSlimFit() {
 
+        // Hitta bild-elementet med hjälp av dess XPath
         WebElement productImage = new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[@class='card-img-top' and contains(@src, 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg')]")));
 
@@ -180,7 +187,7 @@ public class SeleniumTest {
     @Test  //Test för bild src av cotten jackan
     public void testImageShowingForMensCottenJacket() {
 
-
+        // Hitta bild-elementet med hjälp av dess XPath
         WebElement productImage = new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[@class='card-img-top' and contains(@src, 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg')]")));
 
@@ -195,6 +202,7 @@ public class SeleniumTest {
     @Test
     public void testImageIsDisplayingForFjallravenBag() {
 
+        // Hitta bild-elementet med hjälp av dess XPath
         WebElement imageElement = new WebDriverWait(driver, Duration.ofSeconds(15))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"productsContainer\"]/div/div[1]/div/img")));
 
@@ -272,6 +280,7 @@ public class SeleniumTest {
 
     @Test
     public void testCategoryNameWomensClothing() {
+        //Hämta kategorielementen med hjälp av klassnamnet
         WebElement womensClothing = new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div > a.menuLink[href='/category/women\\'s clothing']")));
 
@@ -287,11 +296,11 @@ public class SeleniumTest {
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        // Find and click on the electronics category link
+        // hitta och klicka på electronic länken
         WebElement electronicsLink = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("electronics")));
         electronicsLink.click();
 
-        // Verify that there are 6 product items on the page
+        // kolla så det finns 6 produkter på den sidan
         List<WebElement> products = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("productItem")));
         assertEquals(6, products.size());
 
@@ -306,11 +315,11 @@ public class SeleniumTest {
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        // Find and click on the electronics category link
-        WebElement electronicsLink = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("jewelery")));
-        electronicsLink.click();
+        // hutta och klicka på jewelry länken
+        WebElement jewelery = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("jewelery")));
+        jewelery.click();
 
-        // Verify that there are 6 product items on the page
+        // kolla så det finns 6 produkter på sidan
         List<WebElement> products = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("productItem")));
 
         assertEquals(4, products.size());
@@ -323,11 +332,11 @@ public class SeleniumTest {
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        // Find and click on the electronics category link
-        WebElement electronicsLink = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("men's clothing")));
-        electronicsLink.click();
+        // hitta och klicka på men's clothing länken
+        WebElement mensClothing = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("men's clothing")));
+        mensClothing.click();
 
-        // Verify that there are 6 product items on the page
+        // kolla så det finns 4 produkter på sidan
         List<WebElement> products = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("productItem")));
         assertEquals(4, products.size());
 
@@ -340,11 +349,11 @@ public class SeleniumTest {
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        // Find and click on the electronics category link
-        WebElement electronicsLink = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("women's clothing")));
-        electronicsLink.click();
+        // hitta och klicka på womensclothing
+        WebElement womensClothing = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("women's clothing")));
+        womensClothing.click();
 
-        // Verify that there are 6 product items on the page
+        // kolla så det finns 6 produkter på sidan
         List<WebElement> products = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("productItem")));
         assertEquals(6, products.size());
 
@@ -401,9 +410,11 @@ public class SeleniumTest {
         // Hämta det totala antalet produkter och jämför med det förväntade antalet
         List<WebElement> products = driver.findElements(By.className("productItem"));
 
+        //testar så det finns totalt 20 produkter
         assertEquals(20, products.size(), "Antalet produkter stämmer inte");
 
     }
 }
+
 
 */
